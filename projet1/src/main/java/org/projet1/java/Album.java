37becef41d314add5214a2f1fc26ce796f4c7d6c
Vayou <valentinantoine@hotmail.fr>
2014-01-07 16:57:59 +0100

    maj folder

---
+++ projet1/src/main/java/org/projet1/java/Album.java
@@ -0,0 +1,49 @@
+package org.projet1.java;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Album {
+	
+	private Integer codeAlbum;
+	
+	private String nom;
+	
+	private Set<Chanson> listeChansons;
+	
+	public Album(Integer codeAlbum, String nom) {
+		this.codeAlbum = codeAlbum;
+		this.nom = nom;
+		this.listeChansons = new HashSet<Chanson>();
+		
+	}
+
+	public Integer getCodeAlbum() {
+		return codeAlbum;
+	}
+
+	public void setCodeAlbum(Integer codeAlbum) {
+		this.codeAlbum = codeAlbum;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Set<Chanson> getListeChansons() {
+		return listeChansons;
+	}
+
+	public void setListeChansons(Set<Chanson> listeChansons) {
+		this.listeChansons = listeChansons;
+	}
+	
+	
+	
+	
+
+}
